25832978547fe998acbaf4823760dbb4cb588a33
diego <diego@DESKTOP-VBD0RNK>
2023-09-07 14:44:40 -0300

    Adicionar Controller

---
+++ 02 - Servicos/DDDTreino.Servico.Api/Controllers/LivroController.cs
@@ -0,0 +1,11 @@
+﻿using DDDTreino.Aplicao.DTO;
+using DDDTreino.Aplicao.Interfaces;
+using DDDTreino.Dominio.Entidades;
+
+namespace DDDTreino.Servico.Api.Controllers
+{
+    public class LivroController : ControllerBase<Livro, LivroDTO>
+    {
+        public LivroController(ILivroApp app) : base(app) { }
+    }
+}
